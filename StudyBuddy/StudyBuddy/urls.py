e30c5487690e9a239fde713aface0396213dd87b
Ali Alhaddad <alialhaddad311@gmail.com>
2019-05-07 15:56:21 -0400

    Made login page, made admin account, made separate user, etc

--- StudyBuddy/StudyBuddy/urls.py
+++ StudyBuddy/StudyBuddy/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from server import views as server_views
+
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^accounts/profile/$', server_views.profile),
+    url('r^logout/', auth_views.logout, {'next_page': '/login/$'})
+
+
+
 ]
